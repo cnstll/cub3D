c70bf1e4821533441e9c2e049741b40c79257f2a
Constant ALLE <calle@e2r10p2.42.fr>
2021-02-09 13:27:45 +0100

    playing with xpm images

--- mvp.c
+++ mvp.c
@@ -19,6 +19,8 @@ typedef struct  s_data {
 	void		*win;
 	int			x;
 	int			y;
+	int			x_screen;
+	int			y_screen;
 	int			keycode;
 }               t_data;
 
@@ -30,132 +32,81 @@ void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
     *(unsigned int*)dst = color;
 }
 
-void	print_vertical_line(t_data *data, int x, int y_max, int color)
+void init_img(t_data *data)
 {
-	int		y;
 
-	y = 0;
-	while (y < y_max)
-	{
-		y += 1;
-		my_mlx_pixel_put(data, x, y, color);
-    }
+	data->img = mlx_new_image(data->mlx, data->x_screen, data->y_screen);
+   	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, &data->line_length,
+                                &data->endian);
 }
 
-int             key_hook(int keycode, t_data data)
+int             key_hook(int keycode, t_data *data)
 {
-    printf("keycode is %d\n", keycode);
-	data.keycode = keycode;
-	if (keycode == 13)
+	data->keycode = keycode;
+	if (data->keycode == 13 || data->keycode == 1 || data->keycode == 0 || data->keycode == 2)
 	{
-		data.y -= 5;
-		my_mlx_pixel_put(&data, data.x, data.y, 0x00FF0000);	
-		printf("You moved forward\n");
+		mlx_destroy_image(data->mlx, data->img);
+		init_img(data);
 	}
-    if (keycode == 1)
-	{
-		printf("You moved backward\n");
-	}
-    if (keycode == 0)
-	{
-		printf("You moved left\n");
-	}
-    if (keycode == 2)
-	{
-		printf("You moved right\n");
-	}
-    if (keycode == 124)
-	{
-		printf("You moved your sight to the right\n");
-	}
-    if (keycode == 123)
-	{
-		printf("You moved your sight to the left\n");
-	}
-	if (keycode == 53)
-	{
-		printf("You ESC the game\n");
-	}
-}
-
-int		mouse_hook(int mouse, int x, int y, t_data data)
-{
-    printf("mouse is %d\n", mouse);
-    printf("mouse position %d:%d\n", x, y);
-	if (mouse  == 1)
-	{
-		printf("Left Click\n");
-	}
-	if (mouse  == 2)
-	{
-		printf("Right Click\n");
-	}
-}
-
-int		close (int keycode, t_data *data)
-{
-	if (keycode == 53)
-	{
-		printf("You ESC the game\n");
-		mlx_destroy_window(data->mlx, data->win);
-//		mlx_loop_end(data->mlx);
-	}
-}
-
-/*void		resize(int *sizex, int *sizey, t_data *data)
-{
-	mlx_get_screen_size(data->mlx, sizex, sizey);
-	printf("Window is of size %d:%d\n", *sizex, *sizey);
-}*/
-
-int		render_next_frame(t_data *data)
-{
-	
 	if (data->keycode == 13)
 	{
-		my_mlx_pixel_put(data, data->x, data->y - 10, 0x00FF0000);	
+		data->y -= 10;
+		my_mlx_pixel_put(data, data->x, data->y, 0x00FF0000);	
 		printf("You moved forward\n");
 
 	}
     if (data->keycode == 1)
 	{
-		my_mlx_pixel_put(data, data->x, data->y + 10, 0x00FF0000);	
+		data->y += 10;
+		my_mlx_pixel_put(data, data->x, data->y, 0x00FF0000);	
 		printf("You moved backward\n");
 	}
     if (data->keycode == 0)
 	{
-		my_mlx_pixel_put(data, data->x - 10, data->y, 0x00FF0000);	
+		data->x -= 10;
+		my_mlx_pixel_put(data, data->x, data->y, 0x00FF0000);	
 		printf("You moved left\n");
 	}
     if (data->keycode == 2)
 	{
-		my_mlx_pixel_put(data, data->x + 10, data->y, 0x00FF0000);	
+		data->x += 10;
+		my_mlx_pixel_put(data, data->x, data->y, 0x00FF0000);	
 		printf("You moved right\n");
 	}
-
+	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
 }
 
 int main(void)
 {
 	t_data img;
-
-	img.x = 320;
-	img.y = 240;
+	int width;	
+	int height;
+	char map[8][8] = {	{'1', '1', '1', '1', '1', '1', '1', '1'},
+						{'1', '0', '0', '0', '0', '0', '0', '1'},
+						{'1', '0', '1', 'N', '0', '0', '0', '1'},
+						{'1', '0', '1', '0', '0', '0', '0', '1'},
+						{'1', '0', '0', '0', '0', '1', '0', '1'},
+						{'1', '0', '0', '0', '1', '1', '0', '1'},
+						{'1', '0', '0', '0', '0', '0', '0', '1'},
+						{'1', '1', '1', '1', '1', '1', '1', '1'}};
+	img.x = 0;
+	img.y = 0;
+	img.x_screen = 640;
+	img.y_screen = 480;
 	img.mlx = mlx_init();
-	img.win = mlx_new_window(img.mlx, 640, 480, "cub3D");
-	img.img = mlx_new_image(img.mlx, 640, 480);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-                                &img.endian);
-	
-	
-	my_mlx_pixel_put(&img, img.x, img.y, 0x00FF0000);	
-	mlx_key_hook(img.win, key_hook, &img);
-	//mlx_mouse_hook(img.win, mouse_hook, &img);
-	//mlx_hook(img.win, 2, 1L<<0, close, &img);
-	//my_mlx_pixel_put(&img, img.x, img.y, 0x00FF0000);	
-	//my_mlx_pixel_put(&img, 320, 240, 0x00FF0000);	
-	//mlx_loop_hook(img.mlx, render_next_frame, &img);
-	//mlx_loop_hook(img.mlx, render_next_frame, &img);
-	mlx_put_image_to_window(img.mlx, img.win, img.img, 0, 0);
-    mlx_loop(img.mlx);
+	img.win = mlx_new_window(img.mlx, img.x_screen, img.y_screen, "cub3D");
+	init_img(&img);
+	//	mlx_key_hook(img.win, key_hook, &img);
+	img.img = mlx_xpm_file_to_image (img.mlx , "./pacman.xpm", &width, &height);
+	while (img.y < 480) 
+	{	
+		img.x = 0;
+		while (img.x < 640)
+		{	
+			mlx_put_image_to_window(img.mlx, img.win, img.img, img.x, img.y);
+			img.x = img.x + width;
+		}
+		img.y = img.y + height;
+	}
+	mlx_loop(img.mlx);
 }
